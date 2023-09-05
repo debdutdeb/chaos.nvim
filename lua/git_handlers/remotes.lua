1bf205fea90852ae053e1de0f14cc42a17a0b5c9
Debdut Chakraborty <debdutdeb@outlook.com>
2023-09-06 01:30:52 +0530

    incomplete push

---
+++ lua/git_handlers/remotes.lua
@@ -0,0 +1,39 @@
+local utils = require("chaos.utils")
+
+local M = {}
+
+function M.is_git_worktree()
+	return utils.run_system_command_blocking({ "git", "rev-parse", "--is-inside-work-tree" }).code == 0
+end
+
+function M.list_origins()
+	local result = utils.run_system_command_blocking({ "git", "remote", "show" })
+	if result.code ~= 0 then
+		utils.error("failed to list repository origins")
+		return
+	end
+
+	return result.job:result()
+end
+
+function M.get_remote_url_for_origin(origin)
+	local result = utils.run_system_command_blocking({ "git", "remote", "get-url", origin })
+	if result.code ~= 0 then
+		utils.error("failed to get remote url for origin " .. origin)
+		return
+	end
+
+	return result.job:result()
+end
+
+function M.get_commit_hash_for_file(file)
+	local result = utils.run_system_command_blocking({ "git", "log", "-n", "1", "--pretty=format:%H", "--", file })
+	if result.code ~= 0 then
+		utils.error("failed to get commit hash for file " .. file)
+		return
+	end
+
+	return result.job:result()
+end
+
+return M
---
+++ lua/utils/init.lua
@@ -0,0 +1,38 @@
+local ok, Job = pcall(require, "plenary.job")
+if not ok then
+	vim.notify("plenary is required for this plugin to work", vim.log.levels.WARN)
+	return
+end
+
+local M = {}
+
+local function tbl_slice(tbl, first, last)
+	local sliced = {}
+	for i = first or 1, last or #tbl do
+		slice[#sliced + 1] = tbl[i]
+	end
+	return sliced
+end
+
+function M.run_system_command_blocking(cmd)
+	local payload = {}
+
+	Job:new({
+		command = cmd[1],
+		args = tbl_slice(cmd, 2),
+		on_exit = function(job, return_code)
+			payload = {
+				job = job,
+				code = return_code,
+			}
+		end,
+	}):sync()
+
+	return payload
+end
+
+function M.notify_error(msg)
+	vim.notify(msg, vim.log.levels.ERROR)
+end
+
+return M
