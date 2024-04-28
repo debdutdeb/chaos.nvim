040353b339f4f12edbec58d20f709e129aba36c8
Debdut Chakraborty <debdutdeb@outlook.com>
2024-04-28 09:50:59 +0530

    add command for copying image directly to clipboard

--- lua/chaos/fun/init.lua
+++ lua/chaos/fun/init.lua
@@ -1,5 +1,20 @@
 local vim = vim or {} -- shutting up lsp
 
+if vim.uv.os_uname().sysname ~= "Linux" then
+	vim.notify("Freeze commands are currently supported in Linux")
+	return
+end
+
+if not vim.fn.executable("freeze") then
+	vim.notify("freeze command not found")
+	return
+end
+
+if not vim.fn.executable("xclip") then
+	vim.notify("xclip not found")
+	return
+end
+
 ---@return string
 local function get_lines_within_visual_selection()
 	local s_start = vim.fn.getpos("'<")
@@ -20,7 +35,7 @@ end
 ---@field line1 number
 ---@field line2 number
 
----@return string
+---@return string[]
 ---@param opts LineRange
 local function get_lines(opts)
 	if opts.range then -- visual selection
@@ -34,7 +49,54 @@ local function get_lines(opts)
 	return vim.api.nvim_buf_get_lines(0, 0, -1, false)
 end
 
+local uv = vim.uv
+
+---@param lines string[]
+function freeze(lines)
+	local fd = uv.pipe({ nonblock = true }, { nonblock = true }) -- freeze write to and xclip to read from
+
+	local write_to = uv.new_pipe()
+	write_to:open(fd.write)
+
+	local read_from = uv.new_pipe()
+	read_from:open(fd.read)
+
+	uv.spawn('xclip', {
+		-- TODO: target atoms? i don't think I get what it means. Format looks like to be mime type.
+		-- https://sourceforge.net/p/xclip/patches/4/
+		args = { "-quiet", "-selection", "clipboard", "-t", "image/png" },
+		stdio = { read_from, nil, nil },
+	}, function(code, _signal)
+		if code ~= 0 then
+			print("failed to copy to clipboard")
+			return
+		end
+		print('written to clipboard')
+	end)
+
+	local write_file_name = "/proc/" .. uv.getpid() .. "/fd/" .. fd.write
+
+	local job_id = vim.fn.jobstart(
+		{ "/home/debdut/git/freeze/freeze", "-l", vim.bo.filetype, "-o", write_file_name, "-f", "png", "-" }, {
+			on_stderr = function()
+				require("chaos.utils").notify_error("failed to save screenshot to pipe")
+			end,
+			on_exit = function()
+				vim.notify("successfully saved screenshot to pipe")
+				read_from:read_stop()
+				read_from:close()
+				write_to:close()
+			end,
+		})
+	vim.fn.chansend(job_id, lines)
+	vim.fn.chanclose(job_id, "stdin")
+end
+
 vim.api.nvim_create_user_command("Freeze", function(data)
+	freeze(get_lines({ range = data.range ~= 0, line1 = data.line1, line2 = data.line2 }))
+end, { nargs = 0, range = true })
+
+vim.api.nvim_create_user_command("FreezeToFile", function(data)
 	local job_id = vim.fn.jobstart({ "freeze", "-l", vim.bo.filetype, "-o", data.args, "-" }, {
 		on_stderr = function()
 			require("chaos.utils").notify_error("failed to save screenshot to " .. data.args)
